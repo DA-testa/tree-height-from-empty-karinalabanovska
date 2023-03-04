65935a5abee8fe93f60e9c8e739a33c32e179ca8
Karīna Labanovska-Sekača <125558305+karinalabanovska@users.noreply.github.com>
2023-03-04 19:42:15 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -4,50 +4,39 @@ import sys
 import threading
 
 def compute_height(n, parents):
-    # create an array to store the height of each node, initialized to 0
-    heights = [0] * n
-    
-    # iterate over each node
+    # Create an array to represent the tree where each index represents a node and its value is the parent of that node
+    tree = [[] for i in range(n)]
+    root = None
+
+    # Build the tree
     for i in range(n):
-        # if the height of the node hasn't been computed yet
-        if heights[i] == 0:
-            # calculate the height of the node and its ancestors
-            height = 1
-            parent = parents[i]
-            while parent != -1:
-                if heights[parent] != 0:
-                    # if the height of the parent has already been computed,
-                    # use that to calculate the height of this node
-                    height += heights[parent]
-                    break
-                height += 1
-                parent = parents[parent]
-            # store the height of the node in the array
-            heights[i] = height
-    
-    # return the maximum height
-    return max(heights)
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            tree[parent].append(i)
+
+    # Traverse the tree using DFS with a stack to calculate the height of the tree
+    stack = [(root, 1)]
+    max_height = 0
+    while stack:
+        node, height = stack.pop()
+        if not tree[node]:
+            # Leaf node
+            max_height = max(max_height, height)
+        else:
+            for child in tree[node]:
+                stack.append((child, height+1))
+
+    return max_height
 
 def main():
-    # read input from standard input or files
-    if len(sys.argv) == 1:
-        # read input from standard input
-        n = int(input())
-        parents = list(map(int, input().split()))
-    else:
-        # read input from file
-        filename = sys.argv[1]
-        with open(filename, 'r') as f:
-            n = int(f.readline())
-            parents = list(map(int, f.readline().split()))
-    
-    # compute the height of the tree
-    height = compute_height(n, parents)
-    
-    # output the result
-    print(height)
+    n = int(input())
+    parents = list(map(int, input().split()))
 
-# Increase the recursion limit and stack size for large trees
+    print(compute_height(n, parents))
+
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
+
 threading.Thread(target=main).start()
