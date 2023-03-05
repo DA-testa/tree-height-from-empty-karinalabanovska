a064685813360640264f51a021d3521391a452ce
Karīna Labanovska-Sekača <125558305+karinalabanovska@users.noreply.github.com>
2023-03-05 18:09:04 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -4,11 +4,9 @@ import sys
 import threading
 
 def compute_height(n, parents):
-    # Create an array to represent the tree where each index represents a node and its value is the parent of that node
     tree = [[] for i in range(n)]
     root = None
 
-    # Build the tree
     for i in range(n):
         parent = parents[i]
         if parent == -1:
@@ -16,13 +14,12 @@ def compute_height(n, parents):
         else:
             tree[parent].append(i)
 
-    # Traverse the tree using DFS with a stack to calculate the height of the tree
     stack = [(root, 1)]
     max_height = 0
     while stack:
         node, height = stack.pop()
         if not tree[node]:
-            # Leaf node
+
             max_height = max(max_height, height)
         else:
             for child in tree[node]:
@@ -31,10 +28,27 @@ def compute_height(n, parents):
     return max_height
 
 def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
+    # n = int(input())
+    # parents = list(map(int, input().split()))
 
-    print(compute_height(n, parents))
+    # print(compute_height(n, parents))
+    
+    mode = input()
+    if "F" in mode:
+        filename = input()
+        if "a" not in filename:
+            with open (str("test/"+filename), mode = "r") as f:
+                n = int(f.readline())
+                parentOfNode = list(map(int, f.readline().split()))
+        
+        else:
+            print("error")
+    elif "I" in mode:
+        n = int(input())
+        parentOfNode = list(map(int, input().split()))
+    else:
+        print("invalid mode")
+    print(compute_height(n, parentOfNode))
 
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
